c21204e712a2ab61692e29b6ed983d51d422206a
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-08 09:23:07 +0530

    update header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log("currentUser" + currentUser);
   return (
     <div className=" bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -11,13 +14,21 @@ const Header = () => {
           <Link to="/">
             <li className=" hover:bg-slate-300 py-1 px-2 rounded-md">Home</li>
           </Link>
-          <Link to="about">
+          <Link to="/about">
             <li className=" hover:bg-slate-300 py-1 px-2 rounded-md">About</li>
           </Link>
-          <Link to="sign-in">
-            <li className=" hover:bg-slate-300 py-1 px-2 rounded-md">
-              Sign In
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="profilePicture"
+                className=" size-7 rounded-full object-cover"
+              />
+            ) : (
+              <li className=" hover:bg-slate-300 py-1 px-2 rounded-md">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
